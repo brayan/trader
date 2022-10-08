f604159a191d077fa9b972edee945ca536bee65e
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2022-10-08 17:00:13 -0300

    Add a stock exchange model to B3

---
+++ src/domain/model/B3StockExchange.py
@@ -0,0 +1,17 @@
+class B3StockExchange:
+    def __init__(self):
+        self._name = "B3 - Bolsa, Brasil, Balcão"
+        self._opening_time = "10:00"
+        self._closing_time = "17:00"
+    
+    @property
+    def name(self) -> str:
+        return self._name
+    
+    @property
+    def opening_time(self) -> str:
+        return self._opening_time
+    
+    @property
+    def closing_time(self) -> str:
+        return self._closing_time
