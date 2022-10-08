6cb759041a327f88b033d863028e74475e1a0956
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2022-10-08 17:01:22 -0300

    Add integration between Presenter and StockExchangeService

--- src/presentation/Presenter.py
+++ src/presentation/Presenter.py
@@ -1,29 +1,34 @@
-import schedule
-import datetime
+from datetime import datetime
 import time
+import schedule
 from data.datasource.RealEstateRemoteDataSource import RealEstateRemoteDataSource
 from data.datasource.StockRemoteDataSource import StockRemoteDataSource
+from domain.model.B3StockExchange import B3StockExchange
 from domain.model.RealEstate import RealEstate
 from domain.model.Stock import Stock
+from domain.service.StockExchangeService import StockExchangeService
 
 
 class Presenter:
-    def __init__(
-        self, 
-        stock_remote_data_source: StockRemoteDataSource, 
-        real_estate_remote_data_source: RealEstateRemoteDataSource
-    ):
+    def __init__(self,
+                stock_exchange_service: StockExchangeService,
+                stock_remote_data_source: StockRemoteDataSource, 
+                real_estate_remote_data_source: RealEstateRemoteDataSource):
+        self.stock_exchange_service = stock_exchange_service
         self.stock_remote_data_source = stock_remote_data_source
         self.real_estate_remote_data_source = real_estate_remote_data_source
 
     def start(self):
         schedule.every(5).minutes.do(self._get_info)
+        b3 = B3StockExchange()
         while True:
-            schedule.run_pending()
+            if self.stock_exchange_service.is_stock_exchange_open(b3.opening_time, b3.closing_time):
+                schedule.run_pending()
+                
             time.sleep(1)
-    
+
     def _get_info(self):
-        print("Getting Info... " + str(datetime.datetime.now()))
+        print("Getting Info... " + str(datetime.now()))
         print()
         self._get_all_stocks_info()
         self._get_all_real_estate_info()
