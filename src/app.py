764af92a983de80bff51c04ea97984dcecced014
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2022-10-08 17:01:52 -0300

    Inject StockExchangeService on Presenter

--- src/app.py
+++ src/app.py
@@ -1,8 +1,13 @@
 from data.datasource.RealEstateRemoteDataSourceImpl import RealEstateRemoteDataSourceImpl
 from data.datasource.StockRemoteDataSourceImpl import StockRemoteDataSourceImpl
+from domain.service.StockExchangeService import StockExchangeService
 from presentation.Presenter import Presenter
 
+
+stock_exchange_service = StockExchangeService()
 stock_remote_data_source = StockRemoteDataSourceImpl()
 real_estate_remote_data_source = RealEstateRemoteDataSourceImpl()
 
-Presenter(stock_remote_data_source, real_estate_remote_data_source).start()
+Presenter(stock_exchange_service, 
+          stock_remote_data_source, 
+          real_estate_remote_data_source).start()
