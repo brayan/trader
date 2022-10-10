5ff49a6f2ef46dfdc9a92c74af3bc61baabaea4d
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2022-10-10 09:59:23 -0300

    Disable insecure request warnings

--- src/data/datasource/RealEstateRemoteDataSourceImpl.py
+++ src/data/datasource/RealEstateRemoteDataSourceImpl.py
@@ -3,15 +3,20 @@ import bs4
 from data.datasource.RealEstateRemoteDataSource import RealEstateRemoteDataSource
 from data.mapper.RealEstateToInfoMoneyMapper import map_real_estate_to_info_money
 from domain.model.RealEstate import RealEstate
+from urllib3.exceptions import InsecureRequestWarning
+from urllib3 import disable_warnings
 
 
 class RealEstateRemoteDataSourceImpl(RealEstateRemoteDataSource):
     _URL = "https://www.infomoney.com.br/cotacoes/b3/fii/"
 
+    def __init__(self):
+        disable_warnings(InsecureRequestWarning)
+
     def get_real_estate_price(self, realEstate: RealEstate) -> float:
         real_estate_info_money = map_real_estate_to_info_money(realEstate)
         
-        result = requests.get(self._URL + real_estate_info_money.value)
+        result = requests.get(self._URL + real_estate_info_money.value, verify=False)
         soup = bs4.BeautifulSoup(result.text, "html.parser")
         section = soup.find('div', attrs={ 'class':"cotacoes__header-price" })
         price = section.find_next('span').get_text(strip=True)
