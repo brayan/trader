3b99bbfab9e3c0b79addfb252d6b00ecff03c355
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2022-11-05 20:37:39 -0300

    Change closing time from B3

--- src/domain/model/b3_stock_exchange.py
+++ src/domain/model/b3_stock_exchange.py
@@ -2,7 +2,7 @@ class B3StockExchange:
     def __init__(self):
         self._name = "B3 - Bolsa, Brasil, Balcão"
         self._opening_time = "10:00"
-        self._closing_time = "17:00"
+        self._closing_time = "18:00"
 
     @property
     def name(self) -> str:
--- src/domain/service/test_stock_exchange_service.py
+++ src/domain/service/test_stock_exchange_service.py
@@ -6,6 +6,8 @@ from src.domain.service.stock_exchange_service import StockExchangeService
 
 
 class TestStockExchangeService(unittest.TestCase):
+    _opening_time = "10:00"
+    _closing_time = "18:00"
 
     @freeze_time("2022-10-13")
     def test_is_today_weekday_should_return_true_on_weekday(self):
@@ -22,47 +24,35 @@ class TestStockExchangeService(unittest.TestCase):
     @freeze_time("2022-10-13 10:00:00")
     def test_is_stock_exchange_open_should_return_true_on_weekday_and_on_opening_time(self):
         stock_exchange_service = StockExchangeService()
-        opening_time = "10:00"
-        closing_time = "17:00"
-        is_open = stock_exchange_service.is_stock_exchange_open(opening_time, closing_time)
+        is_open = stock_exchange_service.is_stock_exchange_open(self._opening_time, self._closing_time)
         self.assertTrue(is_open)
 
     @freeze_time("2022-10-13 16:59:59")
     def test_is_stock_exchange_open_should_return_true_on_weekday_and_right_before_closing_time(self):
         stock_exchange_service = StockExchangeService()
-        opening_time = "10:00"
-        closing_time = "17:00"
-        is_open = stock_exchange_service.is_stock_exchange_open(opening_time, closing_time)
+        is_open = stock_exchange_service.is_stock_exchange_open(self._opening_time, self._closing_time)
         self.assertTrue(is_open)
 
-    @freeze_time("2022-10-13 17:00:00")
+    @freeze_time("2022-10-13 18:00:00")
     def test_is_stock_exchange_open_should_return_false_on_weekday_and_on_closing_time(self):
         stock_exchange_service = StockExchangeService()
-        opening_time = "10:00"
-        closing_time = "17:00"
-        is_open = stock_exchange_service.is_stock_exchange_open(opening_time, closing_time)
+        is_open = stock_exchange_service.is_stock_exchange_open(self._opening_time, self._closing_time)
         self.assertFalse(is_open)
 
     @freeze_time("2022-10-09")
     def test_is_stock_exchange_open_should_return_false_on_weekend(self):
         stock_exchange_service = StockExchangeService()
-        opening_time = "10:00"
-        closing_time = "17:00"
-        is_open = stock_exchange_service.is_stock_exchange_open(opening_time, closing_time)
+        is_open = stock_exchange_service.is_stock_exchange_open(self._opening_time, self._closing_time)
         self.assertFalse(is_open)
 
     @freeze_time("2022-10-13 09:59:59")
     def test_is_stock_exchange_open_should_return_false_before_opening_time(self):
         stock_exchange_service = StockExchangeService()
-        opening_time = "10:00"
-        closing_time = "17:00"
-        is_open = stock_exchange_service.is_stock_exchange_open(opening_time, closing_time)
+        is_open = stock_exchange_service.is_stock_exchange_open(self._opening_time, self._closing_time)
         self.assertFalse(is_open)
 
-    @freeze_time("2022-10-13 17:00:01")
+    @freeze_time("2022-10-13 18:00:01")
     def test_is_stock_exchange_open_should_return_false_after_closing_time(self):
         stock_exchange_service = StockExchangeService()
-        opening_time = "10:00"
-        closing_time = "17:00"
-        is_open = stock_exchange_service.is_stock_exchange_open(opening_time, closing_time)
+        is_open = stock_exchange_service.is_stock_exchange_open(self._opening_time, self._closing_time)
         self.assertFalse(is_open)
